a0f6f80bb81271c7c31fce6129121a08addcd1eb
FrancoBisciglia <francobisciglia@hotmail.com>
2023-01-01 15:21:38 -0300

    Actualización de comentarios librería sensor ultrasonico.

--- main/ultrasonic_sensor.h
+++ main/ultrasonic_sensor.h
@@ -53,36 +53,9 @@ typedef struct
 
 /*==================[EXTERNAL FUNCTIONS DECLARATION]=========================*/
 
-/**
- * @brief   Función para inicializar el sensor ultrasónico, para el caso en el que se utilicen los pines
- *          de TRIGGER y ECHO unidos como 1 solo pin que sea INPUT y OUTPUT a la vez.
- *
- * @param trigg_and_echo_pin Pin correspondiente al TRIGGER y ECHO del sensor.
- * @return `ESP_OK` on success
- */
 esp_err_t ultrasonic_sensor_init(ultrasonic_sens_t *ultrasonic_sens_descr);
-
-
-/**
- * @brief   Función que devuelve la distancia respecto del sensor en cm.
- *
- * @param trigg_and_echo_pin Pin correspondiente al TRIGGER y ECHO del sensor.
- * @return `ESP_OK` on success
- */
 esp_err_t ultrasonic_measure_distance_cm(ultrasonic_sens_t *ultrasonic_sens_descr, float *distance_cm);
-
-
-/**
- * @brief   Función que, a partir de las medidas de un tanque de almacenamiento de
- *          líquido, devuelve el nivel de líquido del mismo en por unidad.
- *
- * @param tank  Variable que representa las dimensiones del tanque de almacenamiento, como su altura.
- * @param level Variable donde se guarda el nivel de líquido presente en el tanque, en por unidad.
- * 
- * @return `ESP_OK` on success
- */
 esp_err_t ultrasonic_measure_level(ultrasonic_sens_t *ultrasonic_sens_descr, storage_tank_t *tank, float *level);
-
 
 /*==================[END OF FILE]============================================*/
 
