94ff11c5777086f6d5fbd611e266ee117689040b
FrancoBisciglia <francobisciglia@hotmail.com>
2023-01-01 15:51:04 -0300

    Actualización de comentarios librería WiFi.

--- main/WiFi_STA.c
+++ main/WiFi_STA.c
@@ -28,21 +28,18 @@
 #include "lwip/dns.h"
 #include "lwip/sys.h"
 
-
 //==================================| MACROS AND TYPDEF |==================================//
 
 //==================================| INTERNAL DATA DEFINITION |==================================//
 
-//Variable que identifica la cantidad de veces que se reintentó una conexión
+/* Variable que identifica la cantidad de veces que se reintentó una conexión */
 static int retry_num = 0;
 
-//Tag para imprimir información en el LOG.
+/* Tag para imprimir información en el LOG. */
 static const char *TAG = "WiFi Library";
 
-//Event group de WiFi donde se señalizan los distintos eventos WiFi
+/* Event group de WiFi donde se señalizan los distintos eventos WiFi ¨*/
 static EventGroupHandle_t wifi_event_group;
-
-
 
 //==================================| EXTERNAL DATA DEFINITION |==================================//
 
@@ -53,12 +50,21 @@ void ip_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, 
 
 //==================================| INTERNAL FUNCTIONS DEFINITION |==================================//
 
-//Handler de eventos para eventos de WiFi
+/**
+ * @brief   Funcion de handler para eventos del tipo WiFi.
+ * 
+ * @param arg   Argumento pasado como dato al inicializar el Event Group.
+ * @param event_base    Tipo de evento (IP/WIFI).
+ * @param event_id      ID del evento.      
+ * @param event_data    Datos del evento.
+ */
 void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
-    //Controlamos que el evento mediante el que se ingresó al handler sea del tipo WiFi, y que a su vez sea porque
-    //se inicializo el ESP32 en modo STA correctamente
+    /**
+     *  Controlamos que el evento mediante el que se ingresó al handler sea del tipo WiFi, y que a su vez sea porque
+     *  se inicializo el ESP32 en modo STA correctamente.
+     */
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         
         ESP_LOGI(TAG, "Connecting to AP...");
@@ -66,19 +72,25 @@ void wifi_event_handler(void* arg, esp_event_base_t event_base,
         //Mediante esta función, se realiza la conexion efectiva entre el ESP32 en modo STA y el AP
         esp_wifi_connect();
 
-    //En caso de que haya habido un evento del tipo WiFi, y este evento haya sido que se produjo una desconexión del WiFi,
-    //se ingresa al if para intentar reconectarse
+    /**
+     *  En caso de que haya habido un evento del tipo WiFi, y este evento haya sido que se produjo una desconexión del WiFi,
+     *  se ingresa para intentar reconectarse.
+     */
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
         
-        //En caso de que el numero de intentos de reconexión sea menor al establecido, se intenta otra reconexión
+        /**
+         *  En caso de que el numero de intentos de reconexión sea menor al establecido, se intenta otra reconexión. 
+         */
         if (retry_num < MAX_FAILURES) {
             esp_wifi_connect();
             retry_num++;
             ESP_LOGI(TAG, "Retrying to connect to the AP...");
         } 
         
-        //En caso de que se supere el número máximo de intentos, se le informa al loop del event group que hubo un fallo en la
-        //conexión
+        /**
+         *  En caso de que se supere el número máximo de intentos, se le informa al loop del event group que hubo un 
+         *  fallo en la conexión.
+         */
         else {
             xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
             ESP_LOGI(TAG,"Connection to the AP failed.");
@@ -88,20 +100,34 @@ void wifi_event_handler(void* arg, esp_event_base_t event_base,
 }
 
 
-//Handler de eventos para eventos de IP
+
+/**
+ * @brief   Funcion de handler para eventos del tipo IP.
+ * 
+ * @param arg   Argumento pasado como dato al inicializar el Event Group.
+ * @param event_base    Tipo de evento (IP/WIFI).
+ * @param event_id      ID del evento.      
+ * @param event_data    Datos del evento.
+ */
 void ip_event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
-    //Se controla que se haya recibido un evento del tipo IP, y que el mismo sea que se le asigno una IP
-    //correctamente al ESP32
+    /**
+     *  Se controla que se haya recibido un evento del tipo IP, y que el mismo sea que se le asigno una IP
+     *  correctamente al ESP32
+     */
     if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
 
-        //Se crea una variable para obtener el dato del IP asignado y mostrarlo en pantalla
+        /**
+         *  Se crea una variable para obtener el dato del IP asignado y mostrarlo en pantalla
+         */
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
         ESP_LOGI(TAG, "STA IP:" IPSTR, IP2STR(&event->ip_info.ip));
         retry_num = 0;
 
-        //Se le informa al event group que la conexión al WiFi fue exitosa
+        /**
+         *  Se le informa al event group que la conexión al WiFi fue exitosa
+         */
         xEventGroupSetBits(wifi_event_group, WIFI_SUCCES);
     }
 
@@ -111,66 +137,100 @@ void ip_event_handler(void* arg, esp_event_base_t event_base,
 
 //==================================| EXTERNAL FUNCTIONS DEFINITION |==================================//
 
+/**
+ * @brief   Función utilizada para conectarse a una red WiFi existente.
+ * 
+ * @param wifi_network  Estructura que contiene el ID y PASS de la red WiFi.
+ * @return esp_err_t 
+ */
 esp_err_t connect_wifi(wifi_network_t* wifi_network)
 {
 
-    //Se crea una variable para controlar el estado de la conexión
+    /**
+     *  Se crea una variable para controlar el estado de la conexión
+     */
     int status = WIFI_FAILURE;
 
 
 
-    /** INICIALIZACIÓN **/
+    //========================| INICIALIZACIÓN |===========================//
 
-    //Se inicializa el "non volatile storage" (almacenamiento no volatil) de la flash, donde se guardará la configuración de WiFi
+    /**
+     *  Se inicializa el "non volatile storage" (almacenamiento no volatil) de la flash, donde se guardará la configuración de WiFi
+     */
     esp_err_t ret = nvs_flash_init();
 
-    //En caso de que no haya espacio en la sección de NVS de la flash, se la borra y reinicializa
+    /**
+     *  En caso de que no haya espacio en la sección de NVS de la flash, se la borra y reinicializa.
+     */
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
       ret = nvs_flash_init();
     }
 
-    //Se inicializa la interfaz de red del ESP32
+    /**
+     *  Se inicializa la interfaz de red del ESP32.
+     */
     ESP_ERROR_CHECK(esp_netif_init());
 
-    //Se crea un "evento loop" estandar, que es un ciclo interno que se utiliza para controlar eventos, como por ejemplo, conexiones
-    //o desconexiones a red WiFi, o llegada de datos
+    /**
+     *  Se crea un "evento loop" estandar, que es un ciclo interno que se utiliza para controlar eventos, 
+     *  como por ejemplo, conexiones o desconexiones a red WiFi, o llegada de datos.
+     */
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    //Se le indica a la interfaz de red que vamos a utilizar WiFi y en el modo STA, con la configuración estándar o default
+    /**
+     *  Se le indica a la interfaz de red que vamos a utilizar WiFi y en el modo STA, con la configuración 
+     *  estándar o default.
+     * 
+     */
     esp_netif_create_default_wifi_sta();
 
-    //Se crea una variable mediante la cual se realiza la configuración para el inicio de los parametros del WiFi, en este caso, 
-    //se obtiene la configuración estándar
+    /**
+     *  Se crea una variable mediante la cual se realiza la configuración para el inicio de los parametros 
+     *  del WiFi, en este caso, se obtiene la configuración estándar.
+     */
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 
-    //Se carga la configuración obtenida, pero todavía no se inicia el WiFi
+    /**
+     *  Se carga la configuración obtenida, pero todavía no se inicia el WiFi.
+     */
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
 
 
-    /** CONFIGURACIÓN DEL EVENT LOOP **/
+    //========================| CONFIGURACIÓN DEL EVENT LOOP |===========================//
 
-    //Se crea un event group para manejar los eventos que surjan relacionados con el WiFi
+    /**
+     *  Se crea un event group para manejar los eventos que surjan relacionados con el WiFi
+     */
     wifi_event_group = xEventGroupCreate();
 
-    //Se crea una variable que representa una instancia del handler de eventos de WiFi, que luego puede utilizarse para cerrar 
-    //dicho handler
+    /**
+     *  Se crea una variable que representa una instancia del handler de eventos de WiFi, que luego puede 
+     *  utilizarse para cerrar dicho handler.
+     */
     esp_event_handler_instance_t wifi_handler_event_instance;
 
-    //Se instancia el handler de eventos. 
-    //El primer argumento es para determinar si se espera un evento del tipo WiFi o del tipo IP.
-    //El segundo argumento es el tipo de evento específico que se atenderá, en este caso cualquiera. 
-    //El tercer parámetro es la función del handler que atenderá el evento ocurrido. 
-    //El cuarto parámetro son los argumentos que se le pasarán al handler
-    //El quinto parametro es la instancia del handler declarada anteriormente
+    /**
+     *  Se instancia el handler de eventos:
+     * 
+     *  -El primer argumento es para determinar si se espera un evento del tipo WiFi o del tipo IP.
+     *  -El segundo argumento es el tipo de evento específico que se atenderá, en este caso cualquiera.
+     *  -El tercer parámetro es la función del handler que atenderá el evento ocurrido.
+     *  -El cuarto parámetro son los argumentos que se le pasarán al handler.
+     *  -El quinto parametro es la instancia del handler declarada anteriormente.
+     */
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &wifi_event_handler,
                                                         NULL,
                                                         &wifi_handler_event_instance));
 
-    //Se realiza el mismo procedimiento de creacion de instancia de un handler, pero en este caso para eventos del tipo IP
+    /**
+     *  Se realiza el mismo procedimiento de creacion de instancia de un handler, pero en este caso para eventos del tipo IP.
+     * 
+     */
     esp_event_handler_instance_t got_ip_event_instance;
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
@@ -180,9 +240,11 @@ esp_err_t connect_wifi(wifi_network_t* wifi_network)
 
 
 
-    /** INICIO DEL DRIVER DE WIFI **/
+    //========================| INICIO DEL DRIVER DE WIFI |===========================//
 
-    //Se crea la estructura mediante la cual se configuran las variables del WiFi, como el nombre de la red y contraseña
+    /**
+     *  Se crea la estructura mediante la cual se configuran las variables del WiFi, como el nombre de la red y contraseña.
+     */
     wifi_config_t wifi_config = {
         //En este caso, la conexion es del tipo STA (stationary), que es cuando el ESP32 se conecta a una red existente
         .sta = {
@@ -194,41 +256,55 @@ esp_err_t connect_wifi(wifi_network_t* wifi_network)
         },
     };
 
-    //Se deben copiar de esta forma los datos de la red porque sino lanza un error
+    /**
+     *  Se deben copiar de esta forma los datos de la red porque sino lanza un error.
+     */
     strcpy((char*)wifi_config.sta.ssid, wifi_network->ssid);
     strcpy((char*)wifi_config.sta.password, wifi_network->pass);
 
     ESP_LOGI(TAG, "%s", (char*)wifi_config.sta.ssid);
 
 
-    //Establecemos el modo de la conexion WiFi al tipo STA
+    /**
+     *  Establecemos el modo de la conexion WiFi al tipo STA.
+     */
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
-    //Se carga la configuración de WiFi establecida
+    /**
+     *  Se carga la configuración de WiFi establecida.
+     * 
+     */
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
-    //Se inicia el driver de WiFi
+    /**
+     *  Se inicia el driver de WiFi.
+     */
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "WiFi STA mode initialization complete.");
 
 
 
-    /** ESPERA A LA CONEXIÓN **/
+    //========================| ESPERA A LA CONEXIÓN |===========================//
 
-    //Con esta función, se espera a la recepción de los bits indicados por parte el loop de eventos del "event group".
-    //El primer parámetro es la variable que representa el event group
-    //El segundo parametro son los bits que queremos vigilar si se setean
-    //El tercer parametro indica si queremos que se reseteen los bits indicados una vez se detecten
-    //El cuarto parametro indica si queremos quedarnos esperando en este punto a qué se setee alguno de los bits indicados
-    //El quinto parametro indica el tiempo máximo que queremos esperar a esto
+    /**
+     *  Con esta función, se espera a la recepción de los bits indicados por parte el loop de eventos del "event group":
+     * 
+     *  -El primer parámetro es la variable que representa el event group.
+     *  -El segundo parametro son los bits que queremos vigilar si se setean.
+     *  -El tercer parametro indica si queremos que se reseteen los bits indicados una vez se detecten.
+     *  -El cuarto parametro indica si queremos quedarnos esperando en este punto a qué se setee alguno de los bits indicados.
+     *  -El quinto parametro indica el tiempo máximo que queremos esperar a esto.
+     */
     EventBits_t bits = xEventGroupWaitBits( wifi_event_group,
                                             WIFI_SUCCES | WIFI_FAILURE,
                                             pdFALSE,
                                             pdFALSE,
                                             portMAX_DELAY);
 
-    //Chequeamos si la conexión fue exitosa o hubo algún error
+    /**
+     *  Chequeamos si la conexión fue exitosa o hubo algún error.
+     */
     if(bits & WIFI_SUCCES){
 
         ESP_LOGI(TAG, "Connected to AP.");
@@ -246,8 +322,10 @@ esp_err_t connect_wifi(wifi_network_t* wifi_network)
 
     }
 
-    //Luego, sea que hubo un error o se estableció la conexión de forma exitosa, nos desuscribimos del "event group" y de los
-    //handlers
+    /**
+     *  Luego, sea que hubo un error o se estableció la conexión de forma exitosa, nos desuscribimos del 
+     *  "event group" y de los handlers.
+     */
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, got_ip_event_instance));
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_handler_event_instance));
     vEventGroupDelete(wifi_event_group);
--- main/WiFi_STA.h
+++ main/WiFi_STA.h
@@ -13,42 +13,35 @@ extern "C" {
 
 /*==================================[INCLUDES]=============================================*/
 
-
 #include "esp_event.h"
 #include "esp_log.h"
 
 #include "lwip/err.h"
 
-
-
 /*============================[DEFINES AND MACROS]=====================================*/
 
+/* Macros de mensajes de verificación de funcionamiento de la librería. */
 #define WIFI_SUCCES 1 << 0
 #define WIFI_FAILURE 1 << 1
 #define TCP_SUCCES 1 << 0
 #define TCP_FAILURE 1 << 1
 #define MAX_FAILURES 10
 
-//Definimos la red WiFi a la cual nos conectaremos
-#define SSID "Claro2021"
-#define PSWD "Lavalle1402abcd"
-
-/* The event group allows multiple bits for each event, but we only care about two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
+/* Bits de eventos del event group de WiFi. */
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
-
 /**
- * Device descriptor
+ *  Estructura con la información de la red WiFi a conectarse:
+ *  
+ *  -ssid: Nombre de la red WiFi.
+ *  -pass: Contraseña de la red WiFi.
  */
 typedef struct
 {
     char ssid[50]; //Nombre de la red WiFi
     char pass[50]; //Contraseña de la red WiFi
 } wifi_network_t;
-
 
 /*======================[EXTERNAL DATA DECLARATION]==============================*/
 
@@ -57,8 +50,6 @@ typedef struct
 extern void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 extern void ip_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 esp_err_t connect_wifi(wifi_network_t* wifi_network);
-
-
 
 /*==================[END OF FILE]============================================*/
 #ifdef __cplusplus
