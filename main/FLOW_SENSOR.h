69e15bdedab54108ae23a7bdb0d9cf49d62b0c54
FrancoBisciglia <francobisciglia@hotmail.com>
2023-01-01 11:12:09 -0300

    Inicio de confección de librería de sensor de flujo.
    Se crean los archivos source y header de la librería.

---
+++ main/FLOW_SENSOR.h
@@ -0,0 +1,32 @@
+/*
+
+    FLOW sensor library
+
+*/
+
+#ifndef FLOW_SENSOR_H_   /* Include guard */
+#define FLOW_SENSOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[INCLUDES]=============================================*/
+
+#include <stdio.h>
+#include "esp_err.h"
+#include "driver/gpio.h"
+
+/*==================[DEFINES AND MACROS]=====================================*/
+
+/*==================[EXTERNAL DATA DECLARATION]==============================*/
+
+/*==================[EXTERNAL FUNCTIONS DECLARATION]=========================*/
+
+/*==================[END OF FILE]============================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FLOW_SENSOR_H_
