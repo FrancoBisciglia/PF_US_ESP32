4cb40507bc8c5f5df6ff77de7be50a136d1decad
FrancoBisciglia <francobisciglia@hotmail.com>
2023-01-30 21:11:57 -0300

    Cambios en main para pruebas.

--- main/main.c
+++ main/main.c
@@ -6,11 +6,6 @@
 #include "esp_log.h"
 #include "esp_check.h"
 
-#include "TDS_SENSOR.h"
-#include "WiFi_STA.h"
-#include "MQTT_PUBL_SUSCR.h"
-#include "MEF_ALGORITMO_CONTROL_TDS_SOLUCION.h"
-#include "AUXILIARES_ALGORITMO_CONTROL_TDS_SOLUCION.h"
 #include "MCP23008.h"
 
 
@@ -19,26 +14,56 @@ const char *TAG = "MAIN";
 
 void app_main(void)
 {
-    //=======================| CONEXION WIFI |=======================//
-
-    wifi_network_t network = {
-        .ssid = "Claro2021",
-        .pass = "Lavalle1402abcd",
-    };
-
-    connect_wifi(&network);
-
-    //=======================| CONEXION MQTT |=======================//
-
-    esp_mqtt_client_handle_t MainClienteMqtt = NULL;
-
-    mqtt_initialize_and_connect("mqtt://192.168.100.4:1883", &MainClienteMqtt);
-
-    while(!mqtt_check_connection()){vTaskDelay(pdMS_TO_TICKS(100));}
-
-    ESP_ERROR_CHECK_WITHOUT_ABORT(TDS_sensor_init(ADC1_CHANNEL_3));
     ESP_ERROR_CHECK_WITHOUT_ABORT(MCP23008_init());
 
-    ESP_ERROR_CHECK_WITHOUT_ABORT(aux_control_tds_init(MainClienteMqtt));
-    ESP_ERROR_CHECK_WITHOUT_ABORT(mef_tds_init(MainClienteMqtt));
+    // set_relay_state(RELE_1, OFF);
+    // set_relay_state(RELE_2, OFF);
+     set_relay_state(RELE_3, OFF);
+    set_relay_state(RELE_4, OFF);
+    set_relay_state(RELE_5, OFF);
+    // set_relay_state(RELE_6, OFF);
+    // set_relay_state(RELE_7, OFF);
+
+    while(1)
+    {
+        // set_relay_state(RELE_1, ON);
+        // set_relay_state(RELE_2, ON);
+         set_relay_state(RELE_3, ON);
+        set_relay_state(RELE_4, ON);
+        set_relay_state(RELE_5, ON);
+        // set_relay_state(RELE_6, ON);
+        // set_relay_state(RELE_7, ON);
+
+        // ESP_LOGE(TAG, "%i", get_relay_state(RELE_1));
+        // ESP_LOGE(TAG, "%i", get_relay_state(RELE_2));
+        //ESP_LOGE(TAG, "%i", get_relay_state(RELE_3));
+        // ESP_LOGE(TAG, "%i", get_relay_state(RELE_4));
+        //ESP_LOGE(TAG, "%i", get_relay_state(RELE_5));
+        // ESP_LOGE(TAG, "%i", get_relay_state(RELE_6));
+        // ESP_LOGE(TAG, "%i", get_relay_state(RELE_7));
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+
+        // set_relay_state(RELE_1, OFF);
+        // set_relay_state(RELE_2, OFF);
+        set_relay_state(RELE_3, OFF);
+        set_relay_state(RELE_4, OFF);
+        set_relay_state(RELE_5, OFF);
+        // set_relay_state(RELE_6, OFF);
+        // set_relay_state(RELE_7, OFF);
+
+        // ESP_LOGW(TAG, "%i", get_relay_state(RELE_1));
+        // ESP_LOGW(TAG, "%i", get_relay_state(RELE_2));
+        //ESP_LOGW(TAG, "%i", get_relay_state(RELE_3));
+        // ESP_LOGW(TAG, "%i", get_relay_state(RELE_4));
+        //ESP_LOGW(TAG, "%i", get_relay_state(RELE_5));
+        // ESP_LOGW(TAG, "%i", get_relay_state(RELE_6));
+        // ESP_LOGW(TAG, "%i", get_relay_state(RELE_7));
+
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+    }
 }
+
